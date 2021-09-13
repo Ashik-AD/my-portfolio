3d9a1b3cd5279fd118c43309f5ddb4e754ab4ac5
Ashik-AD <looge321@gmail.com>
2021-09-13 13:27:53 +0545

    ☂️  created sectionContext

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './App.css';
+import Nav from './components/nav/Nav';
+
+export const SectionContext = React.createContext();
 
 const App = () => {
+    const [currentSection, setCurrentSection] = useState("main");
+    const handleCurrentSection = (sectionName) => {
+        setCurrentSection(sectionName);
+    }
     return (
-        <div>Hello world!</div>
+        <div className="App w-screen h-screen bg-main  overflow-hidden">
+            <SectionContext.Provider value={{currentSection, handleCurrentSection}}>
+                <div className="container">
+                    <Nav />
+                </div>
+            </SectionContext.Provider>
+        </div>
     )
 }
 export default App;
