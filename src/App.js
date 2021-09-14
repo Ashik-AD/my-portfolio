b781ef20d7f1c2f51af8395a57a2ce4f0b315963
Ashik-AD <looge321@gmail.com>
2021-09-14 20:21:59 +0545

    😇 added prevSlideIndex that track the previuse slide index and used as z-indexing a slide section

--- src/App.js
+++ src/App.js
@@ -1,22 +1,58 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Contact from './components/contact/Contact';
 import Main from './components/main/Main';
 import Nav from './components/nav/Nav';
+import Skills from './components/skill/Skills';
 
 export const SectionContext = React.createContext();
 
 const App = () => {
-    const [currentSection, setCurrentSection] = useState("main");
+    const [prevSlideIndex, setPrevSlideIndex] = useState(0);
+    const [currentSectionIndex, setCurrentSectionIndex] = useState(1);
+    const [slideDirection, setSlideDirection] = useState(null);
+
+    useEffect(() => {
+        if (currentSectionIndex === prevSlideIndex) {
+            return;
+        }
+        else if (currentSectionIndex > prevSlideIndex) {
+            setSlideDirection('slide-down');
+        }
+        else {
+            setSlideDirection('slide-up');
+        }
+    }, [currentSectionIndex, slideDirection, prevSlideIndex]);
+
     const handleCurrentSection = (sectionName) => {
-        setCurrentSection(sectionName);
+        setPrevSlideIndex(currentSectionIndex);
+        setCurrentSectionIndex(sectionName);
     }
+
     return (
-        <div className="App w-screen h-screen bg-main  overflow-hidden">
-            <SectionContext.Provider value={{currentSection, handleCurrentSection}}>
+        <div className="App w-screen h-screen  overflow-hidden">
+            <SectionContext.Provider value={{currentSectionIndex, handleCurrentSection, prevSlideIndex}}>
                 <div className="container">
                     <Nav />
-                    <Main />
                 </div>
+                <Main
+                    slideDirection={currentSectionIndex === 1 ? slideDirection : ''}
+                    isVisible={currentSectionIndex === 1}
+                    prevSlideIndex={prevSlideIndex}
+                    index={1}
+                />
+                <Skills
+                    slideDirection={currentSectionIndex === 2 ? slideDirection : ''}
+                    isVisible={currentSectionIndex === 2}
+                    prevSlideIndex={prevSlideIndex}
+                    index={2}
+                />
+                <Contact
+                    slideDirection={currentSectionIndex === 3 ? slideDirection : ''}
+                    isVisible={currentSectionIndex === 3}
+                    prevSlideIndex={prevSlideIndex}
+                    index={3}
+                />
             </SectionContext.Provider>
         </div>
     )
