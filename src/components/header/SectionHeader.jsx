6871c49989764f61174a43f6d34cc5e5ac6199a5
Ashik-AD <looge321@gmail.com>
2021-09-14 13:07:52 +0545

    🪶  added responsive for smalldevice

--- src/components/header/SectionHeader.jsx
+++ src/components/header/SectionHeader.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 const SectionHeader = props => {
     const { text, styles } = props;
     return (
-        <div className="section-header w-full">
-            <h1 className={styles}>{ text }</h1>
+        <div className="section-header flex flex-col">
+            <h1 className={`sm-text-60 ${styles}`}>{ text }</h1>
             {props.children}
         </div>
     )
 }
+
 export default SectionHeader;
